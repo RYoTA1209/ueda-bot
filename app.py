4e4a3f6315a962054f3691ffcdc3a88ecf78159c
RYoTA <ryota-develop@outlook.com>
2019-06-12 00:28:00 +0900

    バグ修正

--- app.py
+++ app.py
@@ -33,13 +33,13 @@ def callback():
 
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
-    res = ""
+    res = "null"
     for k,v in pattern_dict.items():
         if k in event.message.text:
             res = v
             break
-        if res:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=res))
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=res))
 
 def prepare_dict():
     with open("./static/pattern.txt") as file:
