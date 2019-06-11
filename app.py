6f76373249c17c42b05eeccbe436ba6b3ba5f98d
RYoTA <ryota-develop@outlook.com>
2019-06-11 23:14:58 +0900

    サンプル出来た。

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask
+from flask import Flask,request,abort
+from linebot import LineBotApi,WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import FollowEvent,MessageEvent,TextMessage,TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage,ButtonsTemplate,PostbackTemplateAction,MessageTemplateAction,URITemplateAction
 import os
 
 port = os.environ['PORT']
 
 app = Flask(__name__)
 
+LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+@app.route('/callback',methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
 
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body:"+body)
+
+    try:
+        handler.handle(body,signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='「'+event.message.text + '」ってなんじゃい？'))
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=port)
